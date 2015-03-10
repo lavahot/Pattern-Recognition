c282077c9ce4b890fafb252bf36843efde2dbcd1
Will <will.s.medrano@gmail.com>
2015-03-10 11:12:43 -0700

    Distribution now works with any covariance matrix
    As opposed to only a diagonal

--- Maximum-Likelihood/bayes_classifier.py
+++ Maximum-Likelihood/bayes_classifier.py
@@ -1,69 +1,42 @@
 import numpy as np
 from numpy import linalg
-import copy, math
-import sympy
+import math
 
 # Distribution
 class Distribution:
-    def __init__(self, mu, sigm, prior = 1.0):
-        self.mu = copy.deepcopy(mu)
-        self.sigm = copy.deepcopy(sigm)
-        self.prior = prior
-        self.dims = len(mu)
-        if len(mu) != len(sigm):
-            raise Exception('mu and sigm must have the same number of dims.')
-    def genSamples(self, n):
-        samples = np.empty((n, self.dims))
-        for i in range(n):
-            for j in range(self.dims):
-                samples[i, j] = random.gauss(self.mu[j], self.sigm[j])
-        return samples
-    def getLogProb(self, val):
-        logProb = math.log(self.prior)
-        for i in range(self.dims):
-            logProb += -(val[i] - self.mu[i])**2 / (2 * self.sigm[i]**2) + math.log(1/(self.sigm[i]*math.sqrt(2*math.pi)))
-        return logProb
-    def getLogProbFormula(self):
-        X = []
-        formula = math.log(self.prior)
-        for i in range(self.dims):
-            X.append(sympy.Symbol('x_'+str(i)))
-        for i in range(self.dims):
-            x = X[i]
-            formula = formula - ((x - self.mu[i])**2 / (2 * self.sigm[i]**2)) + math.log(1/(self.sigm[i]*math.sqrt(2*math.pi)))
-        return formula
-    def __str__(self):
-        return 'mu={mu}, sigm={sigm}, prior={prior}'.format(mu=self.mu,
-                                                            sigm=self.sigm,
-                                                            prior=self.prior)
+	def __init__(self, mu, covar, prior=1.0):
+		self.mu = np.array(mu, dtype='float')
+		self.covar = np.array(covar, dtype='float')
+		self.d = len(mu)
+		self.prior = prior
+		#  Dimension checking
+		if len(self.mu.shape) != 1:
+			raise Exception('mu must be a vector.')
+		if len(self.covar.shape) != 2:
+			raise Exception('covar must be a 2D matrix.')
+		if self.covar.shape[0] != self.covar.shape[1]:
+			raise Exception('covar must be a square matrix.')
+		if self.d != self.covar.shape[0]:
+			raise Exception('covar must have the same number of dimensions as the mu vector.')
+	def getLogProb(self, x):
+		if len(x.shape) != 1:
+			raise Exception('x must be a vector.')
+		if x.shape[0] != self.d:
+			raise Exception('x must have the same number of dimensions as the distribution.')
+		x = np.array(x, dtype='float')
+		p_exp = -0.5 * np.dot((x - self.mu), np.dot(linalg.inv(self.covar), (x - self.mu)))
+		p_bas = -1.0 * math.log(((2.0 * math.pi) ** (self.d/2.0)) * (linalg.det(self.covar)** (self.d/2)))
+		p_pri = math.log(self.prior)
+		return p_exp + p_bas + p_pri
+	def __str__(self):
+		return 'mu={mu}, covar={covar}, prior={prior}'.format(mu=self.mu,
+		                                                      covar=self.covar,
+		                                                      prior=self.prior)
 
-# Predict class
-def predictClass(x, classes):
-    bestClass = None
-    bestLogProb = -float('inf')
-    for C in classes:
-        logProb = C.getLogProb(x)
-        if logProb > bestLogProb:
-            bestLogProb = logProb
-            bestClass = C
-    return bestClass
-
-
-# Bounds
-def getBounds(A, B, linspace):
-    equation = A.getLogProbFormula() - B.getLogProbFormula()
-    sols = sympy.solve(equation, 'x_1')
-    lines = []
-    for sol in sols:
-        line = [[], []]
-        lines.append(line)
-        for xVal in linspace:
-            yVal = sol.subs('x_0', xVal)
-            if not 'I' in str(yVal):
-                xVal = float(xVal)
-                yVal = float(yVal)
-                line[0].append(xVal)
-                line[1].append(yVal)
-            pass
-    return lines
-
+def getClass(x, Classes):
+	best = None, float('-inf')
+	for C in Classes:
+		p = C.getLogProb(x)
+		if p > best[1]:
+			best = C, p
+	return best[0]
