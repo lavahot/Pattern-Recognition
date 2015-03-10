aca9fb052ec3f06db6b05a765395b648ba19a342
Will <will.s.medrano@gmail.com>
2015-03-10 12:05:06 -0700

    Added more documentation

--- Maximum-Likelihood/maximum_likelihood.py
+++ Maximum-Likelihood/maximum_likelihood.py
@@ -3,7 +3,7 @@ import numpy as np
 from bayes_classifier import  Distribution
 
 def getMaximumLikelihood(training):
-	''' Returns a Multivariate Gaussian that represents the data.
+	''' Returns a Multivariate Gaussian of type bayes_classifier.Distribution that represents the data.
 	TODO: Impliment the more generic version that does not assume independance across features.
 	Keyword arguments:
 	training -- An numpy array of features. It should be in the form (sample, feature)
