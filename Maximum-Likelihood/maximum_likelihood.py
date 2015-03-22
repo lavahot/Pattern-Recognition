0f1228805afb18a09e0f53c6bb2d7ee1d047c7c7
Will <will.s.medrano@gmail.com>
2015-03-22 14:26:57 -0700

    ML no longer assumes feature independence

--- Maximum-Likelihood/maximum_likelihood.py
+++ Maximum-Likelihood/maximum_likelihood.py
@@ -13,8 +13,8 @@ def getMaximumLikelihood(training):
 		raise Exception('Training data must be an array of vectors.')
 	d = training.shape[1]
 	mu = []
-	covar_diag = []
 	for i in range(d):
 		mu.append(training[:, i].mean())
-		covar_diag.append(training[:, i].var())
-	return Distribution(mu=mu, covar=np.diag(covar_diag))
+	covar = np.dot(training.transpose(), training)
+	covar /= float(d)
+	return Distribution(mu=mu, covar=covar)
