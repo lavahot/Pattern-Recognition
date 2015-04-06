09d903b7dbe490003210e3d3e9f391ca8a899705
Will <will.s.medrano@gmail.com>
2015-04-06 13:56:39 -0700

    Added pca_test

---
+++ Eigenfaces/pca_test.py
@@ -0,0 +1,30 @@
+from pca import *
+from image_manipulation import *
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Get training and testing images
+trainingFiles = glob.glob('fa_L/*.pgm')
+testingFiles = glob.glob('fb_L/*.pgm')
+
+# Get dimension info
+h, w = readImage(trainingFiles[0]).shape
+d = h * w
+n = len(trainingFiles)
+
+# Load images
+trainingVects = np.empty((d, n), dtype='float')
+for i in range(n):
+	img = readImage(trainingFiles[i])
+	x = vectorizeImage(img)
+	trainingVects[:, i] = x
+
+# PCA
+pca = Pca(trainingFiles, 1.0)
+
+# Test mean face
+meanFace = devectorizeImage(pca.meanVect, w, h)
+plt.title('Mean Face')
+plt.imshow(meanFace)
+plt.show()
