39b18e5126efa3f57bb2f828df861477f6f51dbb
Taylor Mansfield <taylor@loadiq.com>
2015-04-10 20:51:03 -0700

    Added more interactivity and saving of mean and eigenfaces.

--- Eigenfaces/pca_test.py
+++ Eigenfaces/pca_test.py
@@ -3,16 +3,18 @@ from image_manipulation import *
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 # Get training and testing images
-trainingFiles = glob.glob('fa_H/*.pgm')
-testingFiles = glob.glob('fb_H/*.pgm')
+res = str(raw_input("Please enter the resolution of faces you'd like to train for (H/l): ") or 'H').upper()
+trainingFiles = glob.glob('fa_'+ res + '/*.pgm')
+testingFiles = glob.glob('fb_' + res + '/*.pgm')
 
 # Get dimension info
 h, w = readImage(trainingFiles[0]).shape
 d = h * w
 n = len(trainingFiles)
-
+k = float(raw_input("Enter the percent of information you wish to keep (0.95): ") or "0.95")
 # Load images
 print('Loading training data')
 trainingVects = np.empty((d, n), dtype='float')
@@ -23,19 +25,25 @@ for i in range(n):
 
 # print "Here are the training vectors:\n", trainingVects
 # PCA
-pca = Pca(trainingVects, 0.95)
+pca = Pca(trainingVects, k)
 
-# Test mean face
+# Make eigenface directory if it doesn't exist.
+if not os.path.exists("ef_" + res):
+	os.makedirs("ef_" + res)
+
+# Display and save mean face
 print "Showing mean face. Rawr!"
 meanFace = devectorizeImage(pca.meanVect, w, h)
 plt.title('Mean Face')
 plt.imshow(meanFace, cmap='gray')
 plt.show()
+writeImage("ef_" + res + "/mean.pgm", meanFace)
 
-# Test first eigenface
+# Display and save K eigenfaces
 print "Showing " + str(pca.k) + " Eigenfaces out of " + str(len(pca.eigenvalues)) + "."
 for i in range(pca.k):
 	eigenFace = devectorizeImage(pca.eigenvectors[:, pca.eigensort[i]], w, h)
 	plt.title('Eigen Face ' + str(i+1))
 	plt.imshow(eigenFace, cmap='gray')
 	plt.show()
+	writeImage("ef_" + res + "/ef" + str(i) + ".pgm", eigenFace)
